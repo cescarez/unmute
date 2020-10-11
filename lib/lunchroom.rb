647b8b8a789d0e6d97f1f9564669cdf3f8b29408
Christabel Sebastian <christabel.escarez@gmail.com>
2020-10-10 22:54:11 -0700

    Wrote constructor tests

--- lib/lunchroom.rb
+++ lib/lunchroom.rb
@@ -1,6 +1,17 @@
 require_relative 'table'
 require_relative 'user'
 
+
 class Lunchroom
+  attr_reader :users, :tables
+
+  def initialize
+    @users = []
+    @tables = []
+  end
+
+  def self.load_users(channel_name)
+    @users = User.get_user_list
+  end
 
 end
