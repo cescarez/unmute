b6a455b5e8e6bfe9e7a5abd18a12fc9dffa441eb
Christabel Sebastian <christabel.escarez@gmail.com>
2020-10-11 10:31:23 -0700

    Added code and tests for constructor and changes to status

--- lib/user.rb
+++ lib/user.rb
@@ -1,10 +1,6 @@
-require 'httparty'
-require_relative 'slack_api_error'
 require_relative 'helper'
 
-USERS_LIST_URL = "https://slack.com/api/users.list"
 VALID_GROUP_SIZES = [:pair, :small_group]
-
 
 class User
   attr_reader :slack_id, :name, :real_name, :is_active, :group_size, :is_confirmed
@@ -27,7 +23,6 @@ class User
     end
   end
 
-
   def change_active_status
     if @is_active
       @is_active = false
@@ -46,26 +41,6 @@ class User
 
   private
 
-  def self.get_user_list(url)
-    query = {token: get_slack_token}
-    response = check_message(HTTParty.get(USERS_LIST_URL, query: query))
-    User.new(slack_id: response["id"], name: response["name"], real_name: response["real_name"], is_active: true, group_size: :pair, is_confirmed: false)
 
-    #TODO: bot to send welcome message and set is_active upon response, and change :group_size to user selection --- webhook?
-  end
-
-  def self.get_slack_token
-    return ENV["SLACK_TOKEN"]
-  end
-
-
-  def self.check_response(response)
-    if response.code != 200 || response["ok"] != true
-      #todo: check Postman for request response -- does endpoint url exist in header? if so, string interpolate in the below error message
-      raise SlackApiError, "Request to API endpoint failed with error code #{response.code} and #{response["error"]}."
-    else
-      return response
-    end
-  end
 
 end
--- test/user_test.rb
+++ test/user_test.rb
@@ -28,4 +28,55 @@ describe "User class" do
 
   end
 
+
+  describe "#change_group_size" do
+
+    before do
+      @new_user = User.new(slack_id: "some_id123", name: "some_username", real_name: "Some Real Name")
+    end
+
+    it "changes group size" do
+      before = @new_user.group_size
+      @new_user.change_group_size
+      after = @new_user.group_size
+
+      expect(after).wont_equal before
+    end
+
+  end
+
+  describe "#change_active_status" do
+
+    before do
+      @new_user = User.new(slack_id: "some_id123", name: "some_username", real_name: "Some Real Name")
+    end
+
+    it "changes active status" do
+      before = @new_user.is_active
+      @new_user.change_active_status
+      after = @new_user.is_active
+
+      expect(after).wont_equal before
+    end
+
+  end
+
+  describe "#change_confirmation_status" do
+
+    before do
+      @new_user = User.new(slack_id: "some_id123", name: "some_username", real_name: "Some Real Name")
+    end
+
+    it "changes confirmation status" do
+      before = @new_user.is_confirmed
+      @new_user.change_active_status
+      after = @new_user.is_confirmed
+
+      expect(after).wont_equal before
+    end
+
+  end
+
+
+
 end
